7f30708170eb6261dcc4b46dbfccfd770da87c1b
Dominik Horvath <dominik.horvath.to@gmail.com>
2025-05-17 11:07:16 +0200

    add deploy compose and customize frontend build image

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     devIndicators: false,
+    basePath: '',
     experimental: {
         serverActions: {
             bodySizeLimit: '1000mb'
